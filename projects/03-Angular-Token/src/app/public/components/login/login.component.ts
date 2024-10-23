a1febe87097b0f8e0e8df74b3ce1398ad94fdf14
Carlos Geovanny German Millan <geovannygermanmillan@192.168.1.28>
2024-10-23 16:35:45 -0700

    login form finished

--- projects/03-Angular-Token/src/app/public/components/login/login.component.ts
+++ projects/03-Angular-Token/src/app/public/components/login/login.component.ts
@@ -1,13 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CustomInputComponent } from '@/components';
+import { AuthService } from '@/services';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
+
+interface LoginForm {
+  email: FormControl<string>,
+  password: FormControl<string>
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CustomInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
-  
+  authService = inject(AuthService);
+
+  loginForm = new FormGroup<LoginForm>({
+    email: new FormControl('',{
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('',{
+      nonNullable: true,
+      validators: [Validators.required]
+    })
+  })
+
+  async onSubmit(){
+    if(this.loginForm.valid){
+      try{
+        await firstValueFrom(this.authService.login(this.loginForm.getRawValue()))
+      }catch(error){console.error(error)}
+    }
+    this.loginForm.reset();
+  }
 }
