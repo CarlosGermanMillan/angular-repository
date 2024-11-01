e96589d95ac9f868ffda326e58f11472d8f141ea
Carlos Geovanny German Millan <geovannygermanmillan@192.168.1.13>
2024-10-31 22:11:40 -0700

    iframe finished

--- projects/04-EsmileClinicaDental/src/app/app.component.ts
+++ projects/04-EsmileClinicaDental/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './src/toolbar/toolbar.component';
 import { WelcomeComponent } from './src/welcome/welcome.component';
 import { SocialMediaComponent } from './src/social-media/social-media.component';
+import { TrailerVideoComponent } from './src/trailer-video';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarComponent, WelcomeComponent, SocialMediaComponent],
+  imports: [RouterOutlet,ToolbarComponent, WelcomeComponent, SocialMediaComponent, TrailerVideoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
